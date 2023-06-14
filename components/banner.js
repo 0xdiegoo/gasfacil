6df220f9d6392eac01780ad6b98ebda62e10ec94
0xdiegoo <diego.paiva1@outlook.com>
2023-06-14 05:30:00 -0300

    a

--- components/banner.js
+++ components/banner.js
@@ -1,28 +1,36 @@
-import { useEffect, useRef } from 'react'
-const Banner= () => {
-    const bannerRef = useRef(null)
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 
-    const atOptions = {
-        key: '5f0f0e9389135fa749af872435322af8',
-        format: 'iframe',
-        height: 50,
-        width: 320,
-        params: {},
-    }
-    useEffect(() => {
-    if (bannerRef.current && !bannerRef.current.firstChild) {
-        const conf = document.createElement('script')
-        const script = document.createElement('script')
-        script.type = 'text/javascript'
-        script.src = `//www.profitabledisplaynetwork.com/${atOptions.key}/invoke.js`
-        conf.innerHTML = `window.atOptions = ${JSON.stringify(atOptions)}`
+const Banner = () => {
+  const router = useRouter();
 
-        bannerRef.current.appendChild(conf)
-        bannerRef.current.appendChild(script)
-    }
-}, [])
+  const atOptions = {
+    key: '5f0f0e9389135fa749af872435322af8',
+    format: 'iframe',
+    height: 50,
+    width: 320,
+    params: {},
+  };
 
-    return <div ref={bannerRef}></div>
-}
+  useEffect(() => {
+    const conf = document.createElement('script');
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = `//www.profitabledisplaynetwork.com/${atOptions.key}/invoke.js`;
+    conf.innerHTML = `window.atOptions = ${JSON.stringify(atOptions)}`;
+
+    document.getElementById('banner-container').appendChild(conf);
+    document.getElementById('banner-container').appendChild(script);
+  }, []);
+
+  return (
+    <div id="banner-container">
+      <Head>
+        <script src={`//www.profitabledisplaynetwork.com/${atOptions.key}/invoke.js`} />
+      </Head>
+    </div>
+  );
+};
 
 export default Banner;
